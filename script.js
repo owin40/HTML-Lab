ba3c3ce42479d5220d0179fa898d185b9fd6154c
amar <ammarfadillah@gmail.com>
2023-04-05 21:56:05 +0700

    Update IdleClicker.html

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-// Variables to keep track of game state
 let money = 0;
 let clickValue = 1;
 let autoValue = 0;
 let autoPriceAuto = 4;
 let autoPriceInc = 2;
 
-// Update the display for money, click value, and auto value
 function updateDisplay() {
   document.getElementById("money").innerText = "Money: " + money + "$";
   document.getElementById("clickValue").innerText =
@@ -17,13 +15,11 @@ function updateDisplay() {
     "Price for auto click: " + autoPriceAuto + "$";
 }
 
-// Increase the player's money by the click value
 function clickMoney() {
   money += clickValue;
   updateDisplay();
 }
 
-// Buy an auto clicker
 function buyAutoClicker() {
   if (money >= autoPriceAuto) {
     money -= autoPriceAuto;
@@ -35,7 +31,6 @@ function buyAutoClicker() {
   }
 }
 
-// Buy an increase clicker
 function buyIncClicker() {
   if (money >= autoPriceInc) {
     money -= autoPriceInc;
@@ -47,29 +42,22 @@ function buyIncClicker() {
   }
 }
 
-// Automatically increase the player's money
 function autoMoney() {
   money += autoValue;
   updateDisplay();
 }
 
-// Call autoMoney() every second
 setInterval(autoMoney, 1000);
 
-// Add event listeners to the click and buy button
 document.getElementById("click-button").addEventListener("click", clickMoney);
 document.getElementById("buy-button").addEventListener("click", buyAutoClicker);
 
-// Update the display when the page first loads
 updateDisplay();
 
-// create an audio object
 const soundEffect = new Audio("assets/music/coin.mp3");
 
-// add a click event listener to the kodok button
 const kodokButton = document.getElementById("kodok");
 kodokButton.addEventListener("click", function () {
-  // play the sound effect
   soundEffect.play();
 });
 
